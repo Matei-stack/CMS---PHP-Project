e14c148ae6a764f150755db5fa4084bc1891eec3
Matei Murgoci <mateimurgoci123#gmail.com>
2024-09-18 16:18:16 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+
+include "includes/header.php";
+include "includes/db.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog Home - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/blog-home.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+
+    <?php
+
+    include "includes/navigation.php";
+
+    ?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                <?php
+
+                $query = "SELECT * FROM posts";
+                $select_all_posts_query = mysqli_query($connection, $query);
+
+
+
+                while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+
+                ?>
+
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
+
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="#"> <?php echo $post_title ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
+                    <hr>
+                    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                    <hr>
+                    <p><?php echo $post_content ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
+
+
+                <?php
+
+                }
+
+                ?>
+
+
+
+
+
+
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+            <?php
+
+            include "includes/sidebar.php";
+
+            ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+        <!-- Footer -->
+
+
+</html>
+<?php
+
+include "includes/footer.php";
+
+?>
